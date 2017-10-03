242549ef2a035aa1a971c8c23667537d17d5ed46
Varsha Gopal <varsha.gopal@mintpayments.com>
2017-10-03 16:39:35 +1100

    Tour of Heros

--- src/main/java/com/example/PingController.java
+++ src/main/java/com/example/PingController.java
@@ -1,14 +1,12 @@
 package com.example;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/ping")
+
 public class PingController {
-	
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping("/ping")	
 	public String ping() {
 		return "pinged";
 	}
